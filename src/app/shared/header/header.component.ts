8d50fb584291ab92ecab848c6ae34ee687e424ae
BurattinA <Alessandro.Burattin@corvallis.it>
2021-07-22 17:49:14 +0200

    Random edits

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -30,7 +30,7 @@ import { Component, OnInit } from '@angular/core';
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
           <div class="site-heading">
-            <h1 class="title"><i class="fa fa-star" [style.color]='red'></i>BOOK</h1>
+            <h1 class="title"><i class="fa fa-star" [style.color]='red'></i>ER CATCALLING</h1>
             <span class="subheading">Pa du fischi er cat calling "Damiano Er Faina"</span>
           </div>
         </div>
